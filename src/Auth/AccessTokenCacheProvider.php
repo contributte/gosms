fb87a36546498b32c642fa354e8e823fcd7c5715
Milan Matějček <milan.matejcek@gmail.com>
2022-11-24 11:05:42 +0100

    remove for loop

--- src/Auth/AccessTokenCacheProvider.php
+++ src/Auth/AccessTokenCacheProvider.php
@@ -25,28 +25,30 @@ class AccessTokenCacheProvider extends AccessTokenClient
 
 	protected function generateAccessToken(Config $config): AccessToken
 	{
-		$accessToken = null; // phpstan
-		for ($i = 0; $i < 2; ++$i) {
-			$accessToken = $this->cache->load($config->getClientId(), function (&$dependecies) use ($config): AccessToken {
-				$token = parent::generateAccessToken($config);
-				$dependecies[Cache::EXPIRE] = $token->getExpiresAt()->getTimestamp();
+		$accessToken = $this->loadAccessToken($config);
 
-				return $token;
-			});
-			assert($accessToken instanceof AccessToken);
-
-			// MemoryStorage does not accept $dependencies in cache
+		// MemoryStorage does not accept $dependencies in cache
+		if ($accessToken->isExpired()) {
+			$this->cache->remove($config->getClientId());
+			$accessToken = $this->loadAccessToken($config);
 			if ($accessToken->isExpired()) {
-				$this->cache->remove($config->getClientId());
-				$accessToken = null;
-			} else {
-				break;
+				throw new RuntimeException('Could not load access token.');
 			}
 		}
 
-		if ($accessToken === null) {
-			throw new RuntimeException('Could not load access token.');
-		}
+		return $accessToken;
+	}
+
+
+	private function loadAccessToken(Config $config): AccessToken
+	{
+		$accessToken = $this->cache->load($config->getClientId(), function (&$dependecies) use ($config): AccessToken {
+			$token = parent::generateAccessToken($config);
+			$dependecies[Cache::EXPIRE] = $token->getExpiresAt()->getTimestamp();
+
+			return $token;
+		});
+		assert($accessToken instanceof AccessToken);
 
 		return $accessToken;
 	}
