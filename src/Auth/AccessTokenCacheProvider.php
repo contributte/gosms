e18382170f7bb30aa13e186065e70c527b580b00
Milan Matějček <milan.matejcek@gmail.com>
2022-12-14 11:17:38 +0100

    The expiration of cache is same like token's the expiration

--- src/Auth/AccessTokenCacheProvider.php
+++ src/Auth/AccessTokenCacheProvider.php
@@ -4,7 +4,6 @@ namespace Contributte\Gosms\Auth;
 
 use Contributte\Gosms\Config;
 use Contributte\Gosms\Entity\AccessToken;
-use Contributte\Gosms\Exception\RuntimeException;
 use Contributte\Gosms\Http\IHttpClient;
 use Nette\Caching\Cache;
 use Nette\Caching\Storage;
@@ -25,26 +24,9 @@ class AccessTokenCacheProvider extends AccessTokenClient
 
 	protected function generateAccessToken(Config $config): AccessToken
 	{
-		$accessToken = $this->loadAccessToken($config);
-
-		// MemoryStorage does not accept $dependencies in cache
-		if ($accessToken->isExpired()) {
-			$this->cache->remove($config->getClientId());
-			$accessToken = $this->loadAccessToken($config);
-			if ($accessToken->isExpired()) {
-				throw new RuntimeException('Could not load access token.');
-			}
-		}
-
-		return $accessToken;
-	}
-
-
-	private function loadAccessToken(Config $config): AccessToken
-	{
 		$accessToken = $this->cache->load($config->getClientId(), function (&$dependecies) use ($config): AccessToken {
 			$token = parent::generateAccessToken($config);
-			$dependecies[Cache::EXPIRE] = $token->getExpiresAt()->getTimestamp();
+			$dependecies[Cache::EXPIRE] = $token->getExpiresAt()->getTimestamp() - AccessToken::PRE_FETCH_SECONDS;
 
 			return $token;
 		});
