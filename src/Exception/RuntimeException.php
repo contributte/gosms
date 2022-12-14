31628ec122d95a62efca1a9fbf1d765170d6373d
Milan Matějček <milan.matejcek@gmail.com>
2022-12-14 11:58:16 +0100

    Revert RuntimeException

---
+++ src/Exception/RuntimeException.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types = 1);
+
+namespace Contributte\Gosms\Exception;
+
+use RuntimeException as GeneralRuntimeException;
+
+class RuntimeException extends GeneralRuntimeException
+{
+
+}
